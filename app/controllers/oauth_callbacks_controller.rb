94d35bdaaf06d5f5ee54667684151344ceaf396b
vostrikov2014  <vostrikov2014@gmail.com>
2020-12-11 16:06:05 +0300

    unit_12_OAuth_Усложненное_задание_исправление_1_забыл_контроллер_почистить

--- app/controllers/oauth_callbacks_controller.rb
+++ app/controllers/oauth_callbacks_controller.rb
@@ -3,7 +3,6 @@ class OauthCallbacksController < Devise::OmniauthCallbacksController
 
   def github
     #render json: request.env['omniauth.auth']
-    #auth_authentication(request.env['omniauth.auth'], request.env['omniauth.auth'].try(:[], 'info').try(:[], 'email'))
     auth_authentication
   end
 
@@ -11,13 +10,6 @@ class OauthCallbacksController < Devise::OmniauthCallbacksController
   #end
 
   def vkontakte
-    #if User.where(vkontakte_name: request_info.try(:[], 'name')).length == 0
-    #  if request_info.try(:[], 'email').blank? && !session[:pre_email]
-    #    redirect_to new_advanced_registration_path and return
-    #  end
-    #end
-
-    #email = session[:pre_email] || User.where(vkontakte_name: request_info.try(:[], 'name')).first&.email
     auth_authentication
   end
 
@@ -26,16 +18,11 @@ class OauthCallbacksController < Devise::OmniauthCallbacksController
   def auth_authentication
     @user = FindForOauthService.new(session['omniauth.auth']).call
     if @user&.persisted?
-      @user.add_vkontakte_name(request_info.try(:[], 'email'))
       sign_in_and_redirect @user, event: :authentication
       set_flash_message(:notice, :success, kind: session['omniauth.auth'].provider) if is_navigational_format?
     else
       redirect_to root_path, alert: 'Something went wrong'
     end
-  end
-
-  def request_info
-    request.env['omniauth.auth'].try(:[], 'info')
   end
 
   def auth_in_session
