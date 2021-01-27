559789e85fd468f3a72b8393be9c95a05976e62e
vostrikov2014  <vostrikov2014@gmail.com>
2021-01-27 09:54:40 +0300

    unit_17-18

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,8 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  config.action_cable.allowed_request_origins = ["http://206.189.110.91"]
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
@@ -63,11 +65,19 @@ Rails.application.configure do
   # it was in production
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'http://206.189.110.91', port: 80 }
-
-  config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: 'http://206.189.110.91' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    user_name: Rails.application.credentials[Rails.env.to_sym][:gmail][:user_name],
+    password: Rails.application.credentials[Rails.env.to_sym][:gmail][:password],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+  #config.action_mailer.delivery_method = :letter_opener
+  #config.action_mailer.perform_deliveries = true
+  #config.action_mailer.raise_delivery_errors = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
